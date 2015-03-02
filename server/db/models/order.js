1f57879c1d76927a46187867bb8840f2883cc9c9
Ben Levine <Benjamin.R.Levine@gmail.com>
2015-03-02 18:08:22 -0500

    updated order schema

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,9 +2,10 @@ var mongoose = require('mongoose');
 
 var orderSchema = new mongoose.Schema({
     order_id: { type : String },
-    products: { type : Array },
+    //products not final, fix before pull request
+    products: [{ type : Schema.Types.ObjectId, ref: 'Product'}],
     status: { type : Array },
     timestamp: {type: Date, default: Date.now}
 });
 
-mongoose.model('Order', schema);
+mongoose.model('Order', orderSchema);
