07450750620a69b339cf6fce14f79ff10d72b5cf
bosonX <xjin1@binghamton.edu>
2015-03-02 17:47:51 -0500

    Review Schema implemented

---
+++ server/db/models/review.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var schema = mongoose.Schema({
+	rating:{
+		type : Number,
+		min : 1,
+		max : 5
+	},
+	subject:{
+		type : String
+	},
+	content:{
+		type : String
+	},
+	user:[User],
+	time : {
+		type : Date,
+		default : Date.now
+	}
+});
+
+mongoose.model('Review', schema);
