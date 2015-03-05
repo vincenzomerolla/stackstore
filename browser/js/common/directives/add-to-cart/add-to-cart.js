9cf709d9421fcec331cf8c1222ce2ee3e5525eea
Vincenzo Merolla <vincenzomerolla@aol.com>
2015-03-05 18:05:00 -0500

    Progress made on product page

--- browser/js/common/directives/add-to-cart/add-to-cart.js
+++ browser/js/common/directives/add-to-cart/add-to-cart.js
@@ -3,14 +3,19 @@
 app.directive('addToCart', function(){
   // Runs during compile
   return {
-    restrict: 'E',
-    transclude: true,
+    restrict: 'A',
+    //transclude: true,
     //require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-    templateUrl: 'js/common/directives/add-to-cart/add-to-cart.html',
+    //templateUrl: 'js/common/directives/add-to-cart/add-to-cart.html',
     scope: {
       product: '='
     },
-    link: function(scope, element, attrs) {
+    link: function(scope, element, attrs, ctrl, transcludeFn) {
+
+      element.on('click', function(event) {
+        event.preventDefault();
+        console.log('Added to cart');
+      });
       
     }
   };
--- browser/js/product-detail/product-detail.js
+++ browser/js/product-detail/product-detail.js
@@ -4,15 +4,38 @@ app.config(function ($stateProvider) {
     $stateProvider.state('product-detail', {
         url: '/products/:_id',
         controller: 'ProductsDetailCtrl',
-        templateUrl: 'js/product-detail/product-detail.html'
+        templateUrl: 'js/product-detail/product-detail.html',
+        resolve: {
+          product: function($stateParams, Product) {
+            return Product.get($stateParams).$promise;
+          }
+        }
     });
 
 });
 
-app.controller('ProductsDetailCtrl', function ($scope, $stateParams, Product) {
-  Product.get($stateParams).$promise.then(function(product) {
-    $scope.product = product;
-  })
+app.controller('ProductsDetailCtrl', function ($scope, $sce, product) {
+  
+  $scope.product = product;
+
+
+  $scope.tabs = [
+    {
+      title: 'Description',
+      content: $sce.trustAsHtml($scope.product.description)
+    },
+    {
+      title: 'Reviews',
+      content: $sce.trustAsHtml('<h1>here go the reviews.</h1>')
+    },
+    {
+      title: 'Related Products',
+      content: $sce.trustAsHtml('<img class="img-responsive" src="//placehold.it/350x150"/>')
+    }
+  ];
+  $scope.tabs.activeTab = 0;
+
+
 });
 
 
