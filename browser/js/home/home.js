e0854a53c9d86e0332aa0696f254ce1823e8c3b2
Ben Levine <Benjamin.R.Levine@gmail.com>
2015-03-05 18:12:28 -0500

    fixed random game bug

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -22,17 +22,24 @@ app.controller('HomeCtrl', function ($scope, Product) {
         //contains random games by platform selected
         var return_Array = [];
 
-
+        var array_Check = []
         for (var i = 0; i < number_run; i++){
             if (products[i].platform === "Xbox One") {
-                platform_Array.push(products[i])
+                platform_Array.push(products[i]);
             }
         }
 
 
         for (var i = 0; i < number_show; i++) {
-            return_Array.push(platform_Array[Math.floor(Math.random() * (platform_Array.length-1)) + 1]);
+            if (return_Array.indexOf(products[i]) === -1) {
+                var final_Game = platform_Array[Math.floor(Math.random() * (platform_Array.length - 1)) + 1];
+                
+                if (return_Array.indexOf(final_Game) === -1) {
+                    return_Array.push(final_Game);
+                }
+            }
         }
         $scope.products = return_Array;
     });
 });
+
