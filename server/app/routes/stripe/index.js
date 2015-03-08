0f9ce216d97e0109abededab4f07ffd23cde9538
bosonX <xjin1@binghamton.edu>
2015-03-08 00:47:22 -0500

    added form to modal, can post payment to stripe, form seems can't take input, tested with preset value

--- server/app/routes/stripe/index.js
+++ server/app/routes/stripe/index.js
@@ -3,30 +3,26 @@ var stripe = require("stripe")("sk_test_CqEJo0MVuOCRmKQwuwgX1oAM");
 
 router.route('/')
 .post(function(req, res, next) {
-	console.log("hi",req.body,req.token)
-	res.send("hi")
-	// Set your secret key: remember to change this to your live secret key in production
-	// See your keys here https://dashboard.stripe.com/account
-
-	// (Assuming you're using express - expressjs.com)
 	// Get the credit card details submitted by the form
-	var stripeToken = request.body.stripeToken;
+	var stripeToken = req.body.stripeToken;
+	var total = req.body.total;
 
 	var charge = stripe.charges.create({
-	  amount: 1000, // amount in cents, again
+	  amount: total, // amount in cents, again
 	  currency: "usd",
 	  source: stripeToken,
 	  description: "payinguser@example.com"
 	}, function(err, charge) {
 	  if (err && err.type === 'StripeCardError') {
 	    // The card has been declined
+	    res.send("card is declined");
+	  }
+	  else if(charge.status == "succeeded"){
+	  	console.log(total +"cents is charged")
+	  	res.send(total + "cents is received")
+	  	// res.redirect("/user");
 	  }
 	});
 })
-
-// router.route('/')
-// .get(function(req, res, next) {
-// 	res.send("hi")
-// })
 
 module.exports = router;
