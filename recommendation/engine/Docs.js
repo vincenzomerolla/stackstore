58040dc39b38be8d500e34823922af88280ce19a
Ben Levine <Benjamin.R.Levine@gmail.com>
2015-03-09 10:09:10 -0400

    Added recommendation engine code

---
+++ recommendation/engine/Docs.js
@@ -0,0 +1,22 @@
+/*
+
+///////////PROCESS FLOW///////////
+
+- establish empty array
+- establish compatible game hash
+- For each game in collection Loop through all other games in collection
+    - Exclude if same game, inventory of comparable is 0, or non-compatible systems
+
+    -Set game id to element 1 of array.
+    -return top 3 matches from all games based on criteria
+
+Age - 30%
+Creator - 10%
+Num_Players -10 %
+price -10 %
+descriptions 40%
+
+
+*/
+
+
---
+++ recommendation/routes/index.js
@@ -0,0 +1,22 @@
+var Promise = require('bluebird');
+var request = Promise.promisifyAll(require('request'));
+var engine = require('../engine/app.js');
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var promise = request.getAsync({ url: 'http://localhost:1337/api/products'});
+    promise.then(function(contents) {
+        var body = JSON.parse(contents[1]);
+
+        var output = engine.recommend(body);
+
+        res.json(output);
+    });
+});
+
+
+module.exports = router;
+
